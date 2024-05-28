6148a75e0e7524fd1c65a09fb438e4c1ea993f54
unknown <Administrator@DESKTOP-VLUTPC6.Amref.int>
2024-05-28 10:03:38 +0300

    routes added successfully

--- src/index.js
+++ src/index.js
@@ -1,13 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import PostDetail from './pages/PostDetail';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import EditPost from './pages/EditPost';
+import CreatePosts from './pages/CreatePosts';
+import Author from './pages/Author';
+import DeletePost from './pages/DeletePost';
+import Logout from './pages/Logout';
+import Dashboard from './pages/Dashboard';
+import UserProfile from './pages/UserProfile';
+import AuthorsPage from './pages/AuthorsPage';
+import CategoryPosts from './pages/CategoryPosts';
+
+const router = createBrowserRouter([
+  {
+     path:"/",
+     element:<Layout/>,
+     errorElement:<errorPge/>,
+     children: [
+     {index:true, element:<Home/>},
+     {path:"/posts/:id ", element:<PostDetail/>},
+     {path:"register", element:<Register/>},
+     {path:"login", element:<Login/>},
+     {path:"authors", element:<Author/> },
+     {path:"posts/:id/edit", element:<EditPost/>},
+     {path:"create", element:<CreatePosts/>},
+     {path:"/delete/:id", element:<DeletePost/>},
+     {path:"posts/:id",element:<PostDetail/>},
+     {path:"logout", element:<Logout/>},
+     {path:"dashboard", element:<Dashboard/>},
+     {path:"profile/:id", element:<UserProfile/>},
+     {path:"posts/users/:id", element:<AuthorsPage/>},
+     {path:"posts/categories/:id", element:<CategoryPosts/>}
+
+
+
+
+
+     ]
+      
+     
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
+    
   </React.StrictMode>
 );
 
