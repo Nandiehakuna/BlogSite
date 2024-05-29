4afcf6c81bb8edaf8b023a393eb6e57980f4c437
unknown <Administrator@DESKTOP-VLUTPC6.Amref.int>
2024-05-29 08:55:21 +0300

    error page feature complete

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+   <section className='error-page'>
+    <div className="center">
+      <Link to="/" className='btn-primary'>Go back to home</Link>
+      <h2>page not found</h2>
+    </div>
+</section>
   )
 }
 
