5108a8c8fc5aff344347c0140e477d607dc41438
unknown <Administrator@DESKTOP-VLUTPC6.Amref.int>
2024-06-10 16:20:48 +0300

    login and register feature complete

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, {  useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+
+  const [userData, setUserData]= useState([
+    {
+      name:'',
+      email:'',
+      password:'',
+      paswword2:''
+
+    }
+  ])
+
+ const inputChangeHandler = (e)=>{
+  setUserData(prevstate =>{
+   return  {...prevstate, [e.target.name]:e.target.value}
+  })
+
+ }
+
   return (
-    <div>Login</div>
+    <section className='login'>
+      <h2>sign in</h2>
+      <form action="post" className='login-container '>
+        <h1>error occurred during sign up</h1>
+        
+        <input type="email" name='email' value={userData.email} onChange={inputChangeHandler} placeholder='email ' />
+        <input type="password" name='password' value={userData.password} onChange={inputChangeHandler } placeholder='password ' />
+       
+       <button type='submit ' className='btn-primary'>register</button>
+       <small>Dont have an account  ? <Link to='/login'>sign up </Link></small>
+
+
+      </form>
+
+    </section>
+
   )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react'
+import React, {  useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
 
-  const [userData, setUserData]= useEffect([
+  const [userData, setUserData]= useState([
     {
       name:'',
       email:'',
@@ -12,10 +13,25 @@ const Register = () => {
     }
   ])
 
+ const inputChangeHandler = (e)=>{
+  setUserData(prevstate =>{
+   return  {...prevstate, [e.target.name]:e.target.value}
+  })
+
+ }
+
   return (
     <section className='register'>
+      <h2>sign up</h2>
       <form action="post" className='register-container '>
-        <input type="text" name='name' value={userData.name} onChange={handleOnChange} placeholder='enter name ' />
+        <h1>error occurred during sign up</h1>
+        <input type="text" name='name' value={userData.name} onChange={inputChangeHandler} placeholder='Full Name  ' />
+        <input type="email" name='email' value={userData.email} onChange={inputChangeHandler} placeholder='email ' />
+        <input type="password" name='password' value={userData.password} onChange={inputChangeHandler } placeholder='password ' />
+        <input type="password" name='password2' value={userData.paswword2} onChange={inputChangeHandler } placeholder='comfirm password  ' />
+       <button type='submit ' className='btn-primary'>register</button>
+       <small>Already have an account ? <Link to='/login'>sign in </Link></small>
+
 
       </form>
 
